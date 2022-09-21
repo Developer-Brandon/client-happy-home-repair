356e99e6592662895f4c74f0f968c4f1016370bf
Brandon Lee <brandon5959somi@gmail.com>
2022-09-21 16:35:14 +0900

    - happy-home-repair modal 쪽 디자인 수정

--- src/components/estimate/modal/QuestionAboutCallWayModal.vue
+++ src/components/estimate/modal/QuestionAboutCallWayModal.vue
@@ -16,48 +16,48 @@
           <br />
           <p
             class="font-semi-bold"
-            style="text-align: center; font-size: 28px;"
+            style="text-align: center; font-size: 30px;"
           >
             {{ getCommonCompanyNameWithSpace() }}
           </p>
           <p
             class="font-semi-bold"
-            style="text-align: center; font-size: 28px;"
+            style="text-align: center; font-size: 30px;"
           >
-            {{ getCommonPhoneNumber() }}
+            ☎ {{ getCommonPhoneNumber() }} ☎
           </p>
           <br />
           <p
             style="text-align: center;"
             class="hhr-blue"
           >
-            문자 혹은 통화때에 말씀주시면 좋은 사항
+            <span class="font-semi-bold">문자 혹은 통화때에 말씀주시면 좋은 사항</span>
+            <br />
           </p>
           <br />
+<!--          <span class="hhr-red">꼭 알려주셔야 한다는 것이 아니라, 알려주시면 비교적 정확하게 답변드리기에 좋습니다. 감사합니다.</span>-->
+          <br />
           <div style="text-align: center;">
-            <p style="font-size:20px;">
-              1.위치
+            <p style="font-size:17px; line-height:10px;">
+              ✔ 위치
             </p>
             <br />
-            <p style="font-size:20px;">
-              2.작업받으실 종류
+            <p style="font-size:17px; line-height:10px;">
+              ✔ 작업받으실 종류
             </p>
             <br />
-            <p style="font-size:20px;">
-              3.문제점
+            <p style="font-size:17px; line-height:10px;">
+              ✔ 문제점
             </p>
             <br />
-            <p style="font-size:20px;">
-              4.그외 궁금하신점
+            <p style="font-size:17px; line-height:10px;">
+              ✔ 그외 궁금하신점
             </p>
             <br />
-            <p style="font-size:20px;">
-              5.사진
+            <p style="font-size:17px; line-height:10px;">
+              ✔ 사진
             </p>
           </div>
-          <br />
-          <br />
-          <span class="hhr-red">* 꼭 알려주셔야 한다는 것이 아니라, 알려주시면 비교적 정확하게 답변드리기에 좋습니다. 감사합니다.</span>
           <br />
           <br />
           <br />
@@ -136,7 +136,7 @@ export default {
             margin: auto;
             //
             width: 500px;
-            height: 470px;
+            height: 420px;
             padding: 30px;
             background-color: $hhr-white;
             border: 1px solid $hhr-white;
@@ -144,7 +144,7 @@ export default {
             overflow: visible;
             @media (max-width: $screen-mobile) {
                 width: 90%;
-                height: 90%;
+                height: 40%;
                 max-height: 440px;
                 padding: 10px;
                 overflow-x: hidden;
